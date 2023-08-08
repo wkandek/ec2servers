1ea92a64f3c1b03ef8f8db77f95c652ddcb19669
Wolfgang Kandek <wolfgangkandek@Wolfgangs-MacBook-Pro.local>
2023-08-08 07:55:40 -0700

    changed the index

--- keys.py
+++ keys.py
@@ -1,10 +1,6 @@
 awsid = {}
 awskey = {}
 awsregion = {}
-awsid["prod1"] = "AKIAY...7A"
-awskey["prod1"] = "gSJ...S"
-awsregion["prod1"] = "us-east-1"
-awsid["prod2"] = "A...ZYR7" 
-awskey["prod2"] = "K...Eev/kivZ"
-awsregion["prod2"] = "us-west-1"
-
+awsid["prod"] = "AKIA..WT"
+awskey["prod"] = "17Un..C6x"
+awsregion["prod"] = "us-east-1"
